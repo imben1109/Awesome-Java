fd6b4d1998fee00c96e6b9431a1de0106c54dd23
Ben <imben1109@gmail.com>
2025-02-19 12:09:10 +0000

    update

--- jpa/hibernate-example/app/build.gradle.kts
+++ jpa/hibernate-example/app/build.gradle.kts
@@ -21,6 +21,13 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+    implementation( "org.hibernate.orm:hibernate-core:6.4.9.Final")
+    runtimeOnly("com.h2database:h2:2.1.214")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
